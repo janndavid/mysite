600bf906c26f80ddb38bd1018a406693cc66f898
janndavid <jann-david77@live.dk>
2020-03-11 16:51:48 +0100

    Trying to fix this shit

--- polls/testscript.py
+++
@@ -1,5 +0,0 @@
-# this is a test script
-
-print("if this printed, it works!")
-
-print("Trying to connect from this piece of shit OS called wInDoWs")
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+
+# Dette er den nemme måde at gøre det på
+def index(request):
+    return render(request,'polls/index.html')
